8df908381d9608112419ec1591da7759250abcc0
SoimanVasile <vasilecristiansoiman@gmail.com>
2025-12-01 01:42:08 +0200

    source code

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,6 @@ use crate::clap_arg::clap_arg::search_in_file;
 mod clap_arg;
 fn main(){
     let arg = clap_arg::clap_arg::ArgsCommand::parse();
-    search_in_file(arg);
+    let _ =search_in_file(arg);
 
 }
